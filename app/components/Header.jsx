e2489c0625ca2f3a88dc16f5c5cf47bfc9ae923b
Sarai Gabriela <103385050+saraiherrerab@users.noreply.github.com>
2025-07-10 01:39:55 -0400

    Agregando animaciones

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -1,19 +1,25 @@
 import Image from 'next/image'
 import React from 'react'
 import { assets } from '@/assets/assets'
+import { motion } from "motion/react"
 
 
 const Header = () => {
   return (
    <div className="w-11/12 max-w-3xl text-center mx-auto h-screen flex flex-col items-center justify-center gap-4 pt-8">
-      <div>
+      <motion.div
+      initial={{y:-20, opacity:0}}
+      whileInView={{y:0, opacity:1}}
+      transition={{duration:0.8, delay:0.3}}>
         <Image src={assets.profile_img} alt="" className="rounded-full w-40" />
-      </div>
-        <h3 className="flex items-end gap-2 text-xl md:text-2xl mb-3 -mt-10 font-Ovo">
+      </motion.div>
+        <motion.h3 initial={{scale:0}}
+          whileInView={{scale:1}}
+          transition={{duration:0.8, type:'spring', stiffness:100}} className="flex items-end gap-2 text-xl md:text-2xl mb-3 -mt-10 font-Ovo">
 
         ¡Hola!, soy Saraí Herrera
         <Image src={assets.hand_icon} alt="" className="w-6" />
-      </h3>
+      </motion.h3>
 
       <h1 className="text-3xl sm:text-6xl lg:text-[66px] font-Ovo leading-tight">
         Full stack developer 
